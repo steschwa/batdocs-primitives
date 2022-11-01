62579f68b6b17c011aeb154653e2368cb63063de
Stefan Schwaighofer <ste.schwaighofer@gmail.com>
2022-11-01 14:27:38 +0100

    test(bd-37): test prevent default flow

--- packages/floating/src/Floating.stories.tsx
+++ packages/floating/src/Floating.stories.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { forwardRef, useEffect } from "react"
 import * as Floating from "./index"
 
 function useLoadTailwind() {
@@ -21,49 +21,88 @@ export const Default = () => {
         <div className="p-20 flex items-center flex-col gap-y-4">
             <Floating.Root>
                 <Floating.Trigger asChild>
-                    <button className="block px-4 h-8 rounded shadow-sm focus:ring-2 ring-offset-2 outline-none bg-gray-100 text-gray-900 ring-blue-400">
-                        Open floating
-                    </button>
+                    <FloatingTrigger />
                 </Floating.Trigger>
 
                 <Floating.Portal>
                     <Floating.Content
-                        className="flex flex-col gap-y-4 p-4 rounded bg-gray-50 shadow border border-solid border-gray-200 outline-none"
+                        className="rounded bg-gray-50 shadow border border-solid border-gray-200 outline-none"
                         offset={5}
                         fitTrigger={false}>
-                        <fieldset>
-                            <label
-                                htmlFor="first-name"
-                                className="text-gray-700 text-sm font-medium">
-                                First name
-                            </label>
-                            <input
-                                id="first-name"
-                                placeholder="First name"
-                                className="block w-full px-4 h-8 rounded shadow-sm bg-white text-gray-900 placeholder:text-gray-600 border border-solid border-gray-200 focus:ring-2 ring-blue-400 outline-none"
-                            />
-                        </fieldset>
-                        <fieldset>
-                            <label
-                                htmlFor="first-name"
-                                className="text-gray-700 text-sm font-medium">
-                                Last name
-                            </label>
-                            <input
-                                id="last-name"
-                                placeholder="Last name"
-                                className="block w-full px-4 h-8 rounded shadow-sm bg-white text-gray-900 placeholder:text-gray-600 border border-solid border-gray-200 focus:ring-2 ring-blue-400 outline-none"
-                            />
-                        </fieldset>
-
-                        <hr />
-
-                        <button className="block px-4 h-8 rounded shadow-sm focus:ring-2 ring-offset-2 outline-none bg-teal-600 text-white ring-teal-400">
-                            Save
-                        </button>
+                        <FloatingContent />
                     </Floating.Content>
                 </Floating.Portal>
             </Floating.Root>
+        </div>
+    )
+}
+
+export const PreventAutofocus = () => {
+    useLoadTailwind()
+
+    return (
+        <div className="p-20 flex items-center flex-col gap-y-4">
+            <Floating.Root>
+                <Floating.Trigger asChild>
+                    <FloatingTrigger />
+                </Floating.Trigger>
+
+                <Floating.Portal>
+                    <Floating.Content
+                        className="rounded bg-gray-50 shadow border border-solid border-gray-200 outline-none"
+                        offset={5}
+                        fitTrigger={false}
+                        onOpenAutoFocus={event => {
+                            event.preventDefault()
+                        }}>
+                        <FloatingContent />
+                    </Floating.Content>
+                </Floating.Portal>
+            </Floating.Root>
+        </div>
+    )
+}
+
+const FloatingTrigger = forwardRef<HTMLButtonElement, Record<string, unknown>>((props, ref) => {
+    return (
+        <button
+            {...props}
+            ref={ref}
+            className="block px-4 h-8 rounded shadow-sm focus:ring-2 ring-offset-2 outline-none bg-gray-100 text-gray-900 ring-blue-400">
+            Open floating
+        </button>
+    )
+})
+
+function FloatingContent() {
+    return (
+        <div className="flex flex-col gap-y-4 p-4">
+            <fieldset>
+                <label htmlFor="first-name" className="text-gray-700 text-sm font-medium">
+                    First name
+                </label>
+                <input
+                    id="first-name"
+                    placeholder="First name"
+                    className="block w-full px-4 h-8 rounded shadow-sm bg-white text-gray-900 placeholder:text-gray-600 border border-solid border-gray-200 focus:ring-2 ring-blue-400 outline-none"
+                />
+            </fieldset>
+            <fieldset>
+                <label htmlFor="first-name" className="text-gray-700 text-sm font-medium">
+                    Last name
+                </label>
+                <input
+                    id="last-name"
+                    placeholder="Last name"
+                    className="block w-full px-4 h-8 rounded shadow-sm bg-white text-gray-900 placeholder:text-gray-600 border border-solid border-gray-200 focus:ring-2 ring-blue-400 outline-none"
+                />
+            </fieldset>
+
+            <hr />
+
+            <button className="block px-4 h-8 rounded shadow-sm focus:ring-2 ring-offset-2 outline-none bg-teal-600 text-white ring-teal-400">
+                Save
+            </button>
         </div>
     )
 }
--- packages/floating/src/__tests__/Floating.test.tsx
+++ packages/floating/src/__tests__/Floating.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import { describe, expect, test, vi } from "vitest"
 import * as Floating from "../index"
@@ -25,7 +25,9 @@ describe("Floating", () => {
             </Floating.Root>,
         )
     })
+})
 
+describe("Floating.open", () => {
     test("should be able to use component in uncontrolled mode", () => {
         render(
             <Floating.Root defaultOpen>
@@ -79,5 +81,310 @@ describe("Floating", () => {
 
         screen.getByText("Inside Content")
         expect(onOpenChangeSpy).toHaveBeenCalledWith(true)
+    })
+})
+
+describe("Floating.onOpenAutoFocus", () => {
+    test("should call onOpenAutoFocus if content becomes open", async () => {
+        const user = userEvent.setup()
+        const onOpenAutoFocusSpy = vi.fn()
+
+        render(
+            <Floating.Root>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onOpenAutoFocus={onOpenAutoFocusSpy}>
+                    Inside Content
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(document.activeElement).toBe(document.body)
+        expect(onOpenAutoFocusSpy).not.toHaveBeenCalled()
+
+        await user.click(screen.getByText("Trigger"))
+
+        await waitFor(() => {
+            expect(onOpenAutoFocusSpy).toHaveBeenCalled()
+        })
+
+        expect(document.activeElement).toBe(screen.getByTestId("content"))
+    })
+
+    test("should focus first focusable children if content becomes open", async () => {
+        const user = userEvent.setup()
+
+        render(
+            <Floating.Root>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content">
+                    <input placeholder="Input" />
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(document.activeElement).toBe(document.body)
+
+        await user.click(screen.getByText("Trigger"))
+
+        await waitFor(() => {
+            expect(document.activeElement).toBe(screen.getByPlaceholderText("Input"))
+        })
+    })
+
+    test("should focus content if onOpenAutoFocus event is prevented", async () => {
+        const user = userEvent.setup()
+        const onOpenAutoFocusSpy = vi.fn((event: Event) => {
+            event.preventDefault()
+        })
+
+        render(
+            <Floating.Root>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onOpenAutoFocus={onOpenAutoFocusSpy}>
+                    <input placeholder="Input" />
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(document.activeElement).toBe(document.body)
+        expect(onOpenAutoFocusSpy).not.toHaveBeenCalled()
+
+        await user.click(screen.getByText("Trigger"))
+
+        await waitFor(() => {
+            expect(onOpenAutoFocusSpy).toHaveBeenCalled()
+        })
+
+        expect(document.activeElement).toBe(screen.getByTestId("content"))
+    })
+})
+
+describe("Floating.onCloseAutoFocus", () => {
+    test("should call onCloseAutoFocus if content becomes closed", () => {
+        const onCloseAutoFocusSpy = vi.fn()
+
+        const { rerender } = render(
+            <Floating.Root open={true}>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onCloseAutoFocus={onCloseAutoFocusSpy}>
+                    Inside Content
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(document.activeElement).toBe(document.body)
+        expect(onCloseAutoFocusSpy).not.toHaveBeenCalled()
+
+        rerender(
+            <Floating.Root open={false}>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onCloseAutoFocus={onCloseAutoFocusSpy}>
+                    Inside Content
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(onCloseAutoFocusSpy).toHaveBeenCalled()
+
+        expect(document.activeElement).toBe(screen.getByText("Trigger"))
+    })
+
+    test("should not focus trigger if onCloseAutoFocus event is prevented", () => {
+        const onCloseAutoFocusSpy = vi.fn((event: Event) => {
+            event.preventDefault()
+        })
+
+        const { rerender } = render(
+            <Floating.Root open={true}>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onCloseAutoFocus={onCloseAutoFocusSpy}>
+                    Inside Content
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(document.activeElement).toBe(document.body)
+        expect(onCloseAutoFocusSpy).not.toHaveBeenCalled()
+
+        rerender(
+            <Floating.Root open={false}>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onCloseAutoFocus={onCloseAutoFocusSpy}>
+                    Inside Content
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(onCloseAutoFocusSpy).toHaveBeenCalled()
+
+        expect(document.activeElement).not.toBe(screen.getByText("Trigger"))
+    })
+})
+
+describe("Floating.onEscapeKeyDown", () => {
+    test("should call onEscapeKeyDown if Escape is used inside content", async () => {
+        const onEscapeKeyDownSpy = vi.fn()
+        const user = userEvent.setup()
+
+        render(
+            <Floating.Root defaultOpen>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onEscapeKeyDown={onEscapeKeyDownSpy}>
+                    <input autoFocus placeholder="Input" />
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(document.activeElement).toBe(screen.getByPlaceholderText("Input"))
+        expect(onEscapeKeyDownSpy).not.toHaveBeenCalled()
+
+        await user.keyboard("[Escape]")
+
+        expect(onEscapeKeyDownSpy).toHaveBeenCalled()
+
+        await waitFor(() => {
+            expect(screen.queryByTestId("content")).toBeNull()
+        })
+    })
+
+    test("should not close content if onEscapeKeyDown event is prevented", async () => {
+        const onEscapeKeyDownSpy = vi.fn((event: React.KeyboardEvent) => {
+            event.preventDefault()
+        })
+        const user = userEvent.setup()
+
+        render(
+            <Floating.Root defaultOpen>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content data-testid="content" onEscapeKeyDown={onEscapeKeyDownSpy}>
+                    <input autoFocus placeholder="Input" />
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        expect(document.activeElement).toBe(screen.getByPlaceholderText("Input"))
+        expect(onEscapeKeyDownSpy).not.toHaveBeenCalled()
+
+        await user.keyboard("[Escape]")
+
+        expect(onEscapeKeyDownSpy).toHaveBeenCalled()
+
+        await waitFor(() => {
+            expect(screen.queryByTestId("content")).not.toBeNull()
+        })
+    })
+})
+
+describe("Floating.trapFocus", () => {
+    test("should trap focus witin content", async () => {
+        const user = userEvent.setup()
+
+        render(
+            <Floating.Root defaultOpen>
+                <Floating.Trigger asChild>
+                    <button>Trigger</button>
+                </Floating.Trigger>
+                <Floating.Content>
+                    <input placeholder="Input" />
+                    <button>Save</button>
+                </Floating.Content>
+            </Floating.Root>,
+        )
+
+        await waitFor(() => {
+            expect(document.activeElement).toBe(screen.getByPlaceholderText("Input"))
+        })
+
+        await user.tab()
+        expect(document.activeElement).toBe(screen.getByText("Save"))
+
+        await user.tab()
+        expect(document.activeElement).toBe(screen.getByPlaceholderText("Input"))
+
+        await user.tab({ shift: true })
+        expect(document.activeElement).toBe(screen.getByText("Save"))
+    })
+})
+
+describe("Floating.onBlurOutside", () => {
+    test("should close content if focus moves outside", async () => {
+        const onBlurOutsideSpy = vi.fn()
+        const user = userEvent.setup()
+
+        render(
+            <div>
+                <button>Outside</button>
+                <Floating.Root defaultOpen>
+                    <Floating.Trigger asChild>
+                        <button>Trigger</button>
+                    </Floating.Trigger>
+                    <Floating.Content data-testid="content" onBlurOutside={onBlurOutsideSpy}>
+                        <input placeholder="Input" />
+                    </Floating.Content>
+                </Floating.Root>
+                ,
+            </div>,
+        )
+
+        expect(onBlurOutsideSpy).not.toHaveBeenCalled()
+
+        await waitFor(() => {
+            expect(document.activeElement).toBe(screen.getByPlaceholderText("Input"))
+        })
+
+        await user.click(screen.getByText("Outside"))
+
+        expect(screen.queryByTestId("content")).toBeNull()
+        expect(onBlurOutsideSpy).toHaveBeenCalledTimes(1)
+    })
+
+    test("should not close content if onBlurOutside event is prevented", async () => {
+        const onBlurOutsideSpy = vi.fn((event: React.FocusEvent) => {
+            event.preventDefault()
+        })
+        const user = userEvent.setup()
+
+        render(
+            <div>
+                <button>Outside</button>
+                <Floating.Root defaultOpen>
+                    <Floating.Trigger asChild>
+                        <button>Trigger</button>
+                    </Floating.Trigger>
+                    <Floating.Content data-testid="content" onBlurOutside={onBlurOutsideSpy}>
+                        <input placeholder="Input" />
+                    </Floating.Content>
+                </Floating.Root>
+            </div>,
+        )
+
+        expect(onBlurOutsideSpy).not.toHaveBeenCalled()
+
+        await waitFor(() => {
+            expect(document.activeElement).toBe(screen.getByPlaceholderText("Input"))
+        })
+
+        await user.click(screen.getByText("Outside"))
+
+        expect(screen.queryByTestId("content")).not.toBeNull()
+        expect(onBlurOutsideSpy).toHaveBeenCalledTimes(1)
     })
 })
